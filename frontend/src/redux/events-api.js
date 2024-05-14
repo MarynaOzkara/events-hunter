ca4974ae3ef771057d2a9792d73daab207c833f1
MarynaOzkara <ozkaramaryna@gmail.com>
2024-05-14 21:35:06 +0300

    fixing base backend url

--- frontend/src/redux/events-api.js
+++ frontend/src/redux/events-api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://events-hunter.onrender.com/api";
+axios.defaults.baseURL = "https://events-hunter-api.onrender.com";
 export const getEvents = async () => {
   try {
-    const data = await axios.get("/events");
+    const data = await axios.get("/api/events");
     return data;
   } catch (error) {
     console.log(error);
